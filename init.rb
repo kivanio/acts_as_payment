9db03c1f2c608e9d798153a4106e1ebac1518229
Kivanio Barbosa <kivanio@gmail.com>
2009-01-09 17:19:04 -0300

    removendo codigo desnecessario, pois o rghost passou a detectar automaticamente o Sistema Operacional

--- init.rb
+++ init.rb
@@ -15,12 +15,3 @@ require 'boleto/bancobrasil'
 require 'boleto/itau'
 require 'retorno/retorno_cbr643'
 
-# Verificação da Plataforma e setando as configurações do RGHOST
-case RUBY_PLATFORM
-when /darwin/
-  RGhost::Config::GS[:path] = '/opt/local/bin/gs'
-when /linux/
-  RGhost::Config::GS[:path] = '/usr/bin/gs'
-when /freebsd/
-  RGhost::Config::GS[:path] = '/usr/local/bin/gs'
-end
